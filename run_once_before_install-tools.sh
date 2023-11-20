8407e93a40664da8f71af2733192b4a266b2d9cb
Miles Possing <>
2023-11-19 19:18:07 -0600

    Updates

--- run_once_before_install-tools.sh
+++ run_once_before_install-tools.sh
@@ -3,3 +3,8 @@
 if [ ! -d "$HOME/.asdf" ]; then
 	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 fi
+
+. "$HOME/.asdf/asdf.sh"
+
+asdf plugin add nodejs
+asdf plugin add deno
